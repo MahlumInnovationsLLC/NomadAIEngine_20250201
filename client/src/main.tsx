86f23fc1202623ee11eea00e811f767b92668907
coltermahlum <38468008-coltermahlum@users.noreply.replit.com>
2025-01-30 19:28:30 +0000

    Agent query: Is the application starting up without the date-fns related errors now?
    Update `@mui/x-date-pickers` dependency to v5.0.20 and adjust related imports.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/46b47950-8491-429d-bb1f-18901647ad16/2a4f5e4f-cd22-41bd-9fa1-31b170b8bcc0.jpg

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
-import { LocalizationProvider } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import App from './App';
 import "./index.css";
